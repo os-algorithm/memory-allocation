d7eaaf98b3d4761f3c3540200e14e688eafcd20f
tqyaaaaang <tqyangoi1@gmail.com>
2019-04-13 13:52:04 +0800

    feat(sim): add base class for simulator
    Add base class MemoryAllocator for simulator

---
+++ src/ffma.h
@@ -0,0 +1,25 @@
+/**
+ * ffma.h
+ * Memory Allocator Base Class
+ * */
+
+/**
+ * abstract base class MemoryAllocator
+ * public methods:
+ * - Constructor ( int _size ): initialize the allocator with size = _size
+ * - malloc ( int size ): Allocate memory with size = size
+ * - free ( int ptr, int size ): free size memory starting from ptr
+ * private members:
+ * - size: size of the memory
+ * */
+class MemoryAllocator
+{
+public:
+	MemoryAllocator ( int _size ) : memory_size ( _size ) {}
+
+	virtual int malloc ( int size ) = 0;
+	virtual int free ( int ptr, int size ) = 0;
+
+private:
+	int memory_size;
+}
